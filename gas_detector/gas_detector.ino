8abd88fde8422f70dd57623350e80ace382ee417
Ian <chenghsi.yang@gmail.com>
2022-12-15 12:03:54 -0800

    adjusting the display and adding the buzzer

--- gas_detector/gas_detector.ino
+++ gas_detector/gas_detector.ino
@@ -12,41 +12,68 @@ byte _2[8] = { // Array of bytes, B stands for binary formatter and the five num
   B11110
 };
 
-byte _3[8] = {
-  B00000,
-  B00000,
-  B00000,
-  B11110,
-  B00100,
-  B11110,
-  B00010,
-  B11100
-};
+  byte _3[8] = {
+    B00000,
+    B00000,
+    B00000,
+    B11110,
+    B00100,
+    B11110,
+    B00010,
+    B11100
+  };
+
+const int buzzer = 3;
 
 void setup() {
-  Serial.begin(9600); // For data testing
+  // Serial.begin(9600); // For data testing
 
   lcd.createChar(0, _2); // Create a custom character
   lcd.createChar(1, _3);
   lcd.begin(16, 2);  // Initializes the LCD screen, specifying the dimensions (width, height) of the display
-  lcd.print("CO");
-  lcd.write(byte(0));
 }
 
 void loop() {
   int MQ3_value = analogRead(A1);
   int MQ7_value = analogRead(A2);
   int MQ135_value = analogRead(A3);
-  // Serial.println("CO2 Level (PPM): ");
-  Serial.println("MQ3: " + String(MQ3_value));
-  Serial.println("MQ7: " + String(MQ7_value));
-  Serial.println("MQ135: " + String(MQ135_value));
-  delay(2000);
 
+  if (MQ135_value > 130 || MQ7_value > 300 || MQ3_value > 500) {
+    beep(1000);
+  }
+
+  // Serial output
+  // Serial.println("MQ3: " + String(MQ3_value));
+  // Serial.println("MQ7: " + String(MQ7_value));
+  // Serial.println("MQ135: " + String(MQ135_value));
+
+  // LCD Output
   lcd.setCursor(0, 0);
   lcd.write("CO");
   lcd.write(byte(0));  // Display the custom character 0
-  lcd.scrollDisplayLeft();
-  delay(500);
+  lcd.print(":");
+  lcd.setCursor(4, 0);
+  lcd.print(MQ135_value);
 
+  lcd.setCursor(0, 1);
+  lcd.write("CO");
+  lcd.print(" :");
+  lcd.setCursor(4, 1);
+  lcd.print(MQ7_value);
+
+  lcd.setCursor(8, 0);
+  lcd.write("HOP:");
+  lcd.setCursor(12, 0);
+  lcd.print(MQ3_value);
+
+  lcd.setCursor(8, 1);
+  lcd.write("(ppm)");
+
+  delay(1000);
+}
+
+void beep(int time) {
+  analogWrite(buzzer, 220);
+  delay(time);
+  analogWrite(buzzer, 0);
 }
